ef99eea45de1d5dfa45ff8e146407f0b25eb503d
Ashley Thompson <ashleythompsonbca@gmail.com>
2020-04-17 22:58:36 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 def display_rainbow(['red','orange','yellow','green','blue','indigo','violet'])
 colors=["R:red","O:orange","Y:yellow","G:green","B:blue","I:indigo","V:violet"]
-puts "#{red[0]},#{orange[1]},#{yellow[2]},#{green[3]},{blue[4]},{indigo[5]},{violet[6]}
+puts "#{red[0]},#{orange[1]},#{yellow[2]},#{green[3]},#{blue[4]},#{indigo[5]},#{violet[6]}
 end
